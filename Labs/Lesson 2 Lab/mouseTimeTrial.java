02ab2a734b3c7b81f9fa7cc96eaf8830070aa63c
perezsergioace <perez.sergio.ace@gmail.com>
2020-10-25 16:46:21 -0400

    removing the extra system.out statements from file

--- Labs/Lesson 2 Lab/mouseTimeTrial.java
+++ Labs/Lesson 2 Lab/mouseTimeTrial.java
@@ -7,7 +7,6 @@ public class mouseTimeTrial {
         // Create mouse variables
         double mouseCuriosity, mouseHunger, mouseSenseOfSmell, mouseCaution, mouseSpeed;
         String mouseName;
-        String mouseTypeInput;
         char mouseType;
 
         // Create a Scanner object
@@ -25,31 +24,18 @@ public class mouseTimeTrial {
         // Get mouse information from user
         System.out.print("How many points for curiosity?: ");
         mouseCuriosity = keyboard.nextDouble();
-        System.out.println(mouseCuriosity);
-
         System.out.print("How many points for hunger?: ");
         mouseHunger = keyboard.nextDouble();
-        System.out.println(mouseHunger);
-
         System.out.print("How many points for sense of smell?: ");
         mouseSenseOfSmell = keyboard.nextDouble();
-        System.out.println(mouseSenseOfSmell);
-
         System.out.print("How many points for caution?: ");
         mouseCaution = keyboard.nextDouble();
-        System.out.println(mouseCaution);
-
         System.out.print("How many points for speed?: ");
         mouseSpeed = keyboard.nextDouble();
-        System.out.println(mouseSpeed);
-
         System.out.print("Enter a name for your mouse: ");
         mouseName = keyboard.next();
-        System.out.println(mouseName);
-
         System.out.print("Enter A, B, or C for mouse type: ");
         mouseType = keyboard.next().charAt(0);
-        System.out.print(mouseType);
 
     }
 }
