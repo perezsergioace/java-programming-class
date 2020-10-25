9f7c7a69749e6f7f3bf8d6ecc421cee57bcfb48e
perezsergioace <perez.sergio.ace@gmail.com>
2020-10-25 15:40:01 -0400

    starting to work on lesson 2 lab

---
+++ Labs/mouseTimeTrial.java
@@ -0,0 +1,11 @@
+// Import Scanner
+import java.util.Scanner;
+
+public class mouseTimeTrial 
+{
+    public static void main(String[] args)
+    {
+        // Create a Scanner object
+        Scanner keyboard = new Scanner(System.in);
+    }
+}
