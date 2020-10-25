644b25b5d0b577643fe8f5e7433044ae992e2a00
perezsergioace <perez.sergio.ace@gmail.com>
2020-10-25 16:37:40 -0400

    finished writing the code thtat will ask the user for mouse information

--- Labs/mouseTimeTrial.java
+++ Labs/mouseTimeTrial.java
@@ -1,11 +1,15 @@
+
 // Import Scanner
 import java.util.Scanner;
 
-public class mouseTimeTrial 
-{
-    public static void main(String[] args)
-    {
+public class mouseTimeTrial {
+    public static void main(String[] args) {
         // Create mouse variables
+        double mouseCuriosity, mouseHunger, mouseSenseOfSmell, mouseCaution, mouseSpeed;
+        String mouseName;
+        String mouseTypeInput;
+        char mouseType;
+
         // Create a Scanner object
         Scanner keyboard = new Scanner(System.in);
 
@@ -17,5 +21,35 @@ public class mouseTimeTrial
         System.out.print("\tCaution\n");
         System.out.print("\tSpeed\n");
         System.out.println("Each quality must have at least one point");
+
+        // Get mouse information from user
+        System.out.print("How many points for curiosity?: ");
+        mouseCuriosity = keyboard.nextDouble();
+        System.out.println(mouseCuriosity);
+
+        System.out.print("How many points for hunger?: ");
+        mouseHunger = keyboard.nextDouble();
+        System.out.println(mouseHunger);
+
+        System.out.print("How many points for sense of smell?: ");
+        mouseSenseOfSmell = keyboard.nextDouble();
+        System.out.println(mouseSenseOfSmell);
+
+        System.out.print("How many points for caution?: ");
+        mouseCaution = keyboard.nextDouble();
+        System.out.println(mouseCaution);
+
+        System.out.print("How many points for speed?: ");
+        mouseSpeed = keyboard.nextDouble();
+        System.out.println(mouseSpeed);
+
+        System.out.print("Enter a name for your mouse: ");
+        mouseName = keyboard.next();
+        System.out.println(mouseName);
+
+        System.out.print("Enter A, B, or c for mouse type: ");
+        mouseType = keyboard.next().charAt(0);
+        System.out.print(mouseType);
+
     }
 }
