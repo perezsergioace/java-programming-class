b02311068e4cb7fa76912c5817728832773fda0e
perezsergioace <perez.sergio.ace@gmail.com>
2020-10-25 22:25:28 -0400

    new solution to the problem. just need to do the calculations when all the input validations are valid inputs. Also cleaning the format of the code

--- Labs/Lesson 2 Lab/mouseTimeTrial.java
+++ Labs/Lesson 2 Lab/mouseTimeTrial.java
@@ -5,10 +5,11 @@ import java.util.Scanner;
 public class mouseTimeTrial {
     public static void main(String[] args) {
         // Create mouse variables
-        double mouseCuriosity, mouseHunger, mouseSenseOfSmell, mouseCaution, mouseSpeed;
+        int mouseCuriosity, mouseHunger, mouseSenseOfSmell, mouseCaution, mouseSpeed, mouseQualityPoints;
         double mouseTypeATime, mouseTypeBTime, mouseTypeCTime;
         String mouseName;
         char mouseType;
+        boolean maxPointsBoolean, minimumMouseQualityBoolean, mouseTypeBoolean;
 
         // Create a Scanner object
         Scanner keyboard = new Scanner(System.in);
@@ -24,25 +25,64 @@ public class mouseTimeTrial {
 
         // Get mouse information from user
         System.out.print("How many points for curiosity?: ");
-        mouseCuriosity = keyboard.nextDouble();
+        mouseCuriosity = keyboard.nextInt();
         System.out.print("How many points for hunger?: ");
-        mouseHunger = keyboard.nextDouble();
+        mouseHunger = keyboard.nextInt();
         System.out.print("How many points for sense of smell?: ");
-        mouseSenseOfSmell = keyboard.nextDouble();
+        mouseSenseOfSmell = keyboard.nextInt();
         System.out.print("How many points for caution?: ");
-        mouseCaution = keyboard.nextDouble();
+        mouseCaution = keyboard.nextInt();
         System.out.print("How many points for speed?: ");
-        mouseSpeed = keyboard.nextDouble();
+        mouseSpeed = keyboard.nextInt();
         System.out.print("Enter a name for your mouse: ");
         mouseName = keyboard.next();
         System.out.print("Enter A, B, or C for mouse type: ");
         mouseType = keyboard.next().charAt(0);
 
-        // Calculate the mouse's time depending on mouse type(A, B, or C)
+        mouseQualityPoints = mouseCuriosity + mouseHunger + mouseSenseOfSmell + mouseCaution + mouseSpeed;
+
+        // Check input validation
+        if (mouseQualityPoints > 15) {
+            System.out.printf("You've used %d points - only 15 are allowed.\n", mouseQualityPoints);
+            maxPointsBoolean = false;
+        } else {
+            maxPointsBoolean = true;
+        }
+
+        if (mouseCuriosity < 1 || mouseHunger < 1 || mouseSenseOfSmell < 1 || mouseCaution < 1 || mouseSpeed > 1) {
+            System.out.print("Each mouse quality must be assigned a minimum value of 1.\n");
+            minimumMouseQualityBoolean = false;
+        } else {
+            minimumMouseQualityBoolean = true;
+        }
+
+        switch (mouseType) {
+            case 'a':
+            case 'A':
+                mouseTypeBoolean = true;
+                break;
+            case 'b':
+            case 'B':
+                mouseTypeBoolean = true;
+                break;
+            case 'c':
+            case 'C':
+                mouseTypeBoolean = true;
+                break;
+            default:
+                System.out.printf("%s is not a valid mouse type. Mouse type must be A, B, or C.", mouseType);
+                mouseTypeBoolean = false;
+        }
+
+        if (maxPointsBoolean == true && minimumMouseQualityBoolean == true && mouseTypeBoolean == true) {
+            System.out.println("These all passed to do the calculations!");
+        }
+
         mouseTypeATime = Math.log(mouseCuriosity) + Math.log(mouseSpeed) / Math.log(2) + Math.log(mouseSenseOfSmell);
         mouseTypeBTime = Math.log(mouseCaution) + Math.log(mouseHunger) / Math.log(2) + Math.log(mouseSenseOfSmell);
         mouseTypeCTime = Math.log(mouseHunger) + Math.log(mouseSpeed) / Math.log(2) + Math.log(mouseCaution);
 
-        // System.out.printf("time through the maze is %.2f seconds.", mouseTypeBTime);
+        // System.out.printf("%s's time through the maze is %.2f seconds.", mouseName,
+        // mouseTypeBTime);
     }
 }
