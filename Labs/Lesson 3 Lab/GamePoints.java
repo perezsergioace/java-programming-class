a8798b1203623823f419dd4e1e272a8658fdd73f
perezsergioace <perez.sergio.ace@gmail.com>
2020-11-03 23:32:38 -0500

    figured out a good portion of the assignment

--- Labs/Lesson 3 Lab/GamePoints.java
+++ Labs/Lesson 3 Lab/GamePoints.java
@@ -5,12 +5,17 @@ public class GamePoints
     public static void main(String[] args)
     {
         // Create variables for each player and the team.
-        int playerOnePoints;
-        int playerTwoPoints;
-        int playerThreePoints;
-        int playerFourPoints;
-        int playerFivePoints;
-        int teamTotalPoints;
+        int playerOnePointsInput;
+        int playerOnePointsTotal = 0;
+        int playerTwoPointsInput;
+        int playerTwoPointsTotal = 0;
+        int playerThreePointsInput;
+        int playerThreePointsTotal = 0;
+        int playerFourPointsInput;
+        int playerFourPointsTotal = 0;
+        int playerFivePointsInput;
+        int playerFivePointsTotal = 0;
+        // int teamTotalPoints;
         int input;
 
         // Create Scanner object for keyboard input.
@@ -20,28 +25,63 @@ public class GamePoints
         System.out.print("Which player (1, 2, 3, 4, 5; -1 to quit): ");
         input = keyboard.nextInt();
 
-        while (input != -1)
+        if (input == -1)
         {
-            switch (input)
+            System.out.println("No scores were entered.");
+        }
+        else
+        {
+            while (input != -1)
             {
-                case 1:
-                    System.out.println("one");
-                    break;
-                case 2:
-                    System.out.println("two");
-                    break;
-                case 3:
-                    System.out.println("three");
-                    break;
-                case 4:
-                    System.out.println("four");
-                    break;
-                case 5:
-                    System.out.println("five");
-                    break;
+                switch (input)
+                {
+                    case 1:
+                        System.out.println("one");
+
+                        System.out.print("How many points did player 1 score? ");
+                        playerOnePointsInput = keyboard.nextInt();
+                        playerOnePointsTotal += playerOnePointsInput;
+                        break;
+                    case 2:
+                        System.out.println("two");
+                        System.out.print("How many points did player 2 score? ");
+                        playerTwoPointsInput = keyboard.nextInt();
+                        playerTwoPointsTotal += playerTwoPointsInput;
+                        break;
+                    case 3:
+                        System.out.println("three");
+                        System.out.print("How many points did player 3 score? ");
+                        playerThreePointsInput = keyboard.nextInt();
+                        playerThreePointsTotal += playerThreePointsInput;
+                        break;
+                    case 4:
+                        System.out.println("four");
+                        System.out.print("How many points did player 4 score? ");
+                        playerFourPointsInput = keyboard.nextInt();
+                        playerFourPointsTotal += playerFourPointsInput;
+                        break;
+                    case 5:
+                        System.out.println("five");
+                        System.out.print("How many points did player 5 score? ");
+                        playerFivePointsInput = keyboard.nextInt();
+                        playerFivePointsTotal += playerFivePointsInput;
+                        break;
+                    default:
+                        System.out.println("That is not a valid player number.");
+                }
+
+                System.out.print("Which player (1, 2, 3, 4, 5; -1 to quit): ");
+                input = keyboard.nextInt();
+
+                if (input == -1)
+                {
+                    System.out.println("Player 1 points - " + playerOnePointsTotal);
+                    System.out.println("Player 2 points - " + playerTwoPointsTotal);
+                    System.out.println("Player 3 points - " + playerThreePointsTotal);
+                    System.out.println("Player 4 points - " + playerFourPointsTotal);
+                    System.out.println("Player 5 points - " + playerFivePointsTotal);
+                }
             }
-            System.out.print("Which player (1, 2, 3, 4, 5; -1 to quit): ");
-            input = keyboard.nextInt();
         }
     }
 }
