c32ec1c026cd832423168f392550dd93f853e5c2
perezsergioace <perez.sergio.ace@gmail.com>
2020-11-03 22:04:54 -0500

    creating scanner object for keyboard input

--- Labs/Lesson 3 Lab/GamePoints.java
+++ Labs/Lesson 3 Lab/GamePoints.java
@@ -4,10 +4,15 @@ public class GamePoints
 {
     public static void main(String[] args)
     {
+        // Create variables for each player and the team
         int playerOnePoints;
         int playerTwoPoints;
         int playerThreePoints;
         int playerFourPoints;
         int playerFivePoints;
+        int teamTotalPoints;
+
+        // Create Scanner object for keyboard input
+        Scanner keyboard = new Scanner(System.in);
     }
 }
