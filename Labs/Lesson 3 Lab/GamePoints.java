091cc541c2fd77e7f1f81c70c63f8625d61a6dd7
perezsergioace <perez.sergio.ace@gmail.com>
2020-11-03 22:24:27 -0500

    created if else if statements to ask the user how many points each user gets

--- Labs/Lesson 3 Lab/GamePoints.java
+++ Labs/Lesson 3 Lab/GamePoints.java
@@ -4,15 +4,52 @@ public class GamePoints
 {
     public static void main(String[] args)
     {
-        // Create variables for each player and the team
+        // Create variables for each player and the team.
         int playerOnePoints;
         int playerTwoPoints;
         int playerThreePoints;
         int playerFourPoints;
         int playerFivePoints;
         int teamTotalPoints;
+        int input;
 
-        // Create Scanner object for keyboard input
+        // Create Scanner object for keyboard input.
         Scanner keyboard = new Scanner(System.in);
+
+        // Display general instrucitons.
+        System.out.print("Which player (1, 2, 3, 4, 5; -1 to quit): ");
+        input = keyboard.nextInt();
+
+        if (input == 1)
+        {
+            System.out.printf("How many points did player %d score? ", input);
+            playerOnePoints = keyboard.nextInt();
+            System.out.println(playerOnePoints);
+        }
+        else if (input == 2)
+        {
+            System.out.printf("How many points did player %d score? ", input);
+            playerTwoPoints = keyboard.nextInt();
+            System.out.println(playerTwoPoints);
+        }
+        else if (input == 3)
+        {
+            System.out.printf("How many points did player %d score? ", input);
+            playerThreePoints = keyboard.nextInt();
+            System.out.println(playerThreePoints);
+        }
+        else if (input == 4)
+        {
+            System.out.printf("How many points did player %d score? ", input);
+            playerFourPoints = keyboard.nextInt();
+            System.out.println(playerFourPoints);
+        }
+        else if (input == 5)
+        {
+            System.out.printf("How many points did player %d score? ", input);
+            playerFivePoints = keyboard.nextInt();
+            System.out.println(playerFivePoints);
+        }
+        
     }
 }
