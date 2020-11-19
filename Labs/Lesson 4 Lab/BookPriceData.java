f7c861cda17b149b0c5a72c617124796b4bd7d53
perezsergioace <perez.sergio.ace@gmail.com>
2020-11-18 23:18:41 -0500

    built out the function that will check if the file that the user enters exists or not. If it does not exist, the program will output a message letting the user know the file does not exist and will exit out the program

--- Labs/Lesson 4 Lab/BookPriceData.java
+++ Labs/Lesson 4 Lab/BookPriceData.java
@@ -1,5 +1,5 @@
-import java.util.Scanner;
-import java.io.*;
+import java.util.Scanner;	// Needed for the Scanner class
+import java.io.*;		// Needed for the File and IOException
 
 /**
  * This program reads book price data from a file, performs some computations, and displays a table of cost data.
@@ -9,6 +9,39 @@ public class BookPriceData
 {
 	public static void main(String[] args) throws IOException
 	{
-		System.out.println("This is the BookPriceData java file.");
+		// Create a Scanner object for keyboard input.
+		Scanner keyboard = new Scanner(System.in);
+
+		// Get the file name.
+		System.out.print("What is the name of the file? ");
+		String filename = keyboard.nextLine();
+
+		// Make sure the file exists.
+		File file = new File(filename);
+		if (!file.exists())
+		{
+			System.out.printf("The file %s was not found.\n", file);
+		}
+
+		// Cannot use System.exit
+		if (file.exists() != false)
+		{
+			// Open the file.
+			Scanner inputFile = new Scanner(file);
+
+			// Read lines from the file until no more are left.
+			while (inputFile.hasNext())
+			{
+				// Read the next line of data
+				String bookDataLine = inputFile.nextLine();
+
+				// Display the last line of book data
+				System.out.println(bookDataLine);
+			}
+
+			// Close the file.
+			inputFile.close();
+
+		}
 	}
 }
