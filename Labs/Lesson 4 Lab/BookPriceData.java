8b6adec7d53bf6d46f654b5a82b60f45929f59aa
perezsergioace <perez.sergio.ace@gmail.com>
2020-11-18 22:42:18 -0500

    Creating the lesson 4 lab assignment java file

---
+++ Labs/Lesson 4 Lab/BookPriceData.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+import java.io.*;
+
+/**
+ * This program reads book price data from a file, performs some computations, and displays a table of cost data.
+ */
+
+public class BookPriceData
+{
+	public static void main(String[] args) throws IOException
+	{
+		System.out.println("This is the BookPriceData java file.");
+	}
+}
